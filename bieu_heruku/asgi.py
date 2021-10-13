35f8faa3c5e52cc15718749dd48d694be74171e3
quỳnh <pnhat481@gmail.com>
2021-10-13 17:37:35 +0700

    nhất tải lên

---
+++ bieu_heruku/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bieu_heruku project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bieu_heruku.settings')
+
+application = get_asgi_application()
